7c8e363ca178180b9ddc7f6ce093d7eb84099e17
Li Shu <lishu2@gelib-4c-29.ews.illinois.edu>
2019-12-06 23:30:38 -0600

    first commit

--- model.py
+++ model.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+
+import numpy as np
+from __future__ import print_function
+import time
+
 import torch
 import torch.nn as nn
 import torch.autograd as autograd
@@ -5,8 +11,8 @@ import torch.nn.functional as F
 
 class Model(nn.Module):
     def __init__(self, n_in=[3,210,160], conv_channels=[32, 64, 64],
-                 conv_kernels=[8, 4, 3], conv_strides=[4, 2, 1],  n_atoms=51, 
-                 Vmin=-10., Vmax= 30, n_fc = [128, 256],n_out = 6):
+                 conv_kernels=[8, 4, 3], conv_strides=[4, 2, 1], n_atoms=51,
+                 Vmin=-10., Vmax= 30, n_fc = [128, 256], n_out = 6):
         super(Model, self).__init__()
         #paramters from paper
         self.n_in = n_in
@@ -15,6 +21,8 @@ class Model(nn.Module):
         self.n_atoms = n_atoms
         self.Vmin = Vmin
         self.Vmax = Vmax
+        self.delta_z = (self.Vmax - self.Vmin) / float(self.n_atoms - 1)
+        
 
         c0 = n_in[0]
         h0 = n_in[1]
@@ -30,28 +38,20 @@ class Model(nn.Module):
             c0 = c
         self.conv_layers = nn.Sequential(*self.conv_layers)
 
-        # self.fc = nn.Sequential(
-        #     nn.Linear(self.input_dim[0], 128),
-        #     nn.ReLU(),
-        #     nn.Linear(128, 256),
-        #     nn.ReLU(),
-        #     nn.Linear(256, self.output_dim * self.n_atoms)
-        # )
-
         self.fc_layers = []
         h0 = h0 * h0 * conv_channels[-1]
         for i, h in enumerate(n_fc):
             # append Linear and ReLU layers
-            self.fc_layers.append(nn.Linear(h0, h))            
+            self.fc_layers.append(nn.Linear(h0, h))
             self.fc_layers.append(nn.ReLU())
             h0 = h
-        
+
         self.fc_layers.append(nn.Linear(h, self.n_out*self.n_atoms))
         self.fc_layers.append(nn.Softmax(dim=1))
         self.fc_layers = nn.Sequential(*self.fc_layers)
 
 
-    def forward(self, x, head=None):
+    def forward(self, x):
         x = x.float() / 256
 
         # feed x into the self.conv_layers
